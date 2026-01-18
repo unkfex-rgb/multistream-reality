340c3501c5f8a85eb1cc21c1498b4927548573cb
unkfex-rgb <unkfex@gmail.com>
2026-01-18 07:18:19 -0300

    Delete layout.jsx

--- layout.jsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: 'Multistream Reality' };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-BR">
-      <body style={{ margin: 0, background: '#000' }}>{children}</body>
-    </html>
-  );
-}
